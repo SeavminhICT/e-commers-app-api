f0f12f4ebfb027105f05a73260d887b26c68d0a6
Vicheka321 <longvicheka2004@gmail.com>
2025-06-05 09:24:57 +0700

    first

--- lib/module/home_screen.dart
+++ lib/module/home_screen.dart
@@ -7,13 +7,14 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
+      bottomNavigationBar: _buildNavigationBar(), // add navigation bar here
     );
   }
 
   AppBar _buildAppBar() {
     return AppBar(
       centerTitle: true,
-      title: Text(
+      title: const Text(
         'Mega Mall',
         style: TextStyle(
           fontWeight: FontWeight.bold,
@@ -21,7 +22,6 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       actions: [
-        
         IconButton(
           icon: Image.asset(
             'images/notification_icon.png',
@@ -39,6 +39,105 @@ class HomeScreen extends StatelessWidget {
           onPressed: () {},
         ),
       ],
+    );
+  }
+
+  Widget _buildNavigationBar() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 4,
+            color: Colors.black.withOpacity(0.1),
+            offset: const Offset(0, -1),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'images/Home_icon.png',
+                width: 24,
+                height: 24,
+                color: Colors.blue, // optional color tint
+              ),
+              const SizedBox(height: 4),
+              const Text(
+                'HOME',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'images/wishlist_icon.png',
+                width: 24,
+                height: 24,
+                color: Colors.black,
+              ),
+              const SizedBox(height: 4),
+              const Text(
+                'WISHLIST',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'images/order_icon.png',
+                width: 24,
+                height: 24,
+                color: Colors.black,
+              ),
+              const SizedBox(height: 4),
+              const Text(
+                'ORDER',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              CircleAvatar(
+                radius: 12,
+                backgroundImage: AssetImage('images/profile.jpg'),
+              ),
+              SizedBox(height: 4),
+              Text(
+                'ACCOUNT',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
